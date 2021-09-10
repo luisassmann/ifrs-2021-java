40d21344182a60c4115d3325f2cf86c6eb6249d6
luisdef <luis.assmann.1234@gmail.com>
2021-09-10 09:55:54 -0300

    configs

---
+++ Aula-06A/src/exemplos/Coletivo.java
@@ -0,0 +1,15 @@
+package exemplos;
+
+public class Coletivo {
+	
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa();
+		p1.nome = "Luis";
+		p1.idade = 16;
+		p1.cidadeNascimento = "Bom Princípio";
+
+		p1.imprimirDados();
+
+	}
+}
---
+++ Aula-06A/src/exemplos/Pessoa.java
@@ -0,0 +1,15 @@
+package exemplos;
+
+public class Pessoa {
+
+	public String nome;
+	public int idade;
+	public String cidadeNascimento;
+	
+	public void imprimirDados() {
+		System.out.println(this.nome);
+		System.out.println(this.idade);
+		System.out.println(this.cidadeNascimento);
+	}
+	
+}
