8d91b55aa6269278388c4a3b751c73abea211bed
luisdef <luis.assmann.1234@gmail.com>
2021-12-19 00:02:08 -0300

    exercicio12 menu

--- Exercicios-Lista-12/src/ex1/Exercicio2.java
+++ Exercicios-Lista-12/src/ex1/Exercicio2.java
@@ -1,5 +1,6 @@
 package ex1;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Exercicio2 {
@@ -7,12 +8,14 @@ public class Exercicio2 {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		Random random = new Random();
 		
-		FiguraGeometrica[] fgs = new FiguraGeometrica[0];
+		FiguraGeometrica[] fgs = null;
+		FiguraGeometrica[] copFgs = null;
 		
-		int opt = 1;
+		int opt = 9;
 		do {
-			System.out.println("Formas Geométricas\n");
+			System.out.println("\nTrabalhando com Formas Geométricas\n");
 			System.out.println("Opções:");
 			System.out.println("  - (1) Inserir;");
 			System.out.println("  - (2) Remover;");
@@ -21,19 +24,89 @@ public class Exercicio2 {
 			System.out.print("> ");
 			opt = Integer.parseInt(scan.nextLine());
 			
-			if (opt == 1) {
-				System.out.print("Posição a inserir uma forma geométrica: ");
-				int posicao = Integer.parseInt(scan.nextLine());
-				
-				fgs = new FiguraGeometrica[posicao];
-				
-				System.out.print("Qual FG?\n  - (1) Circulo;\n  - (2) Retângulo\n> ");
-				
-				
-				
+			switch (opt) {
+				case 1: {
+					System.out.print("Posição a inserir uma forma geométrica: ");
+					int posicao = Integer.parseInt(scan.nextLine());
+					
+					if (fgs != null) {
+						if (posicao > fgs.length) {
+							copFgs = fgs;
+							fgs = new FiguraGeometrica[posicao];
+							for (int i = 0; i < copFgs.length; i++) {
+								fgs[i] = copFgs[i];
+							}
+						}
+					} else {
+						fgs = new FiguraGeometrica[posicao];
+					}
+					
+					System.out.print("Qual FG?\n  - (1) Circulo;\n  - (2) Retângulo\n> ");
+					int figura = Integer.parseInt(scan.nextLine());
+					
+					if (figura == 1) {
+						int raioC = random.nextInt(11);
+						String nomeC = String.format("Circulo %d", posicao);
+						fgs[posicao-1] = new Circulo(nomeC,raioC);
+					} else if (figura == 2) {
+						int lado1R = random.nextInt(11);
+						int lado2R = random.nextInt(11);
+						String nomeR = String.format("Retangulo %d", posicao);
+						fgs[posicao-1] = new Retangulo(nomeR, lado1R, lado2R);
+					}
+					break;
+				}
+				case 2: {
+					System.out.print("Posição a remover a forma geométrica: ");
+					int posicaoR = Integer.parseInt(scan.nextLine());
+					
+					if (fgs[posicaoR-1] == null) {
+						System.out.println("Espaço em branco removido.");
+					} else {
+						if (fgs[posicaoR-1].getClass().getName() == "ex1.Circulo") {
+							String nomeC = String.format("Circulo %d", posicaoR);
+							
+							System.out.printf("%s removido.\n", nomeC);
+								
+						} else if (fgs[posicaoR-1].getClass().getName() == "ex1.Retangulo") {
+							String nomeR = String.format("Retangulo %d", posicaoR);
+
+							System.out.printf("%s removido.\n", nomeR);
+						}
+					}
+
+					
+					fgs[posicaoR-1] = null;
+					copFgs = fgs;
+					fgs = new FiguraGeometrica[copFgs.length-1];
+					int iF = 0;
+					for (int i = 0; i < copFgs.length; i++) {
+						if (copFgs[i] != null) {
+							fgs[iF] = copFgs[i];
+							iF++;
+						}
+					}
+					break;
+				}
+				case 3: {
+					System.out.println("Listagem das Formas Geométricas:");
+					
+					System.out.println("-----------------------");
+					for (int i = 0; i < fgs.length; i++) {
+						if (fgs[i] != null) {
+							System.out.printf("  %s\n", fgs[i].getNome());
+							System.out.printf("  - Perímetro: %.1f\n", fgs[i].calculaPerimetro());
+							System.out.printf("  - Área: %.1f\n", fgs[i].calculaArea());
+							System.out.println("-----------------------");
+						}
+					}
+					
+				}
+					
 			}
 			
 		} while (opt != 0);
+		
 		
 		scan.close();
 
